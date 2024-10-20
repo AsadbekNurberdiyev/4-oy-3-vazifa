5734bc55ebb2ffe9ecc107f4a83685a0b4ce2dce
AsadbekNurberdiyev <asadbeknurberdiyev31@gmail.com>
2024-10-21 01:18:06 +0500

    amallar bajardim

---
+++ style/main.js
@@ -0,0 +1,579 @@
+// let a=+prompt("yoshni kiriting")
+// if (a>0 && a<=3) {
+//     alert("chaqalaoq")
+// }else if(a>=4 && a<=7){
+//     alert("go'dak")
+// } else if (a>7 && a<=12) {
+//     alert("bola")
+// }else if(a>12 && a<=18){
+//     alert("o'spirin")
+// } else if (a>18 && a<=21) {
+//     alert("o'smir")
+// }else if (a>21) {
+//     alert("erkak")
+// }else {
+//     alert("hali planda")
+// }
+
+                                // 2-masala
+// let k=+prompt("bahoni kiriting")
+// switch (k) {
+//     case 1:
+//         console.log("yomon");
+        
+//     break;
+//         case 2:
+//             console.log("qoniqarsiz");
+            
+//             break;
+//             case 3:
+//                 console.log("qoniqarli");
+                
+//                 break;
+//                 case 4:
+//                     console.log("yaxshi");
+                    
+//                     break;
+//                     case 5:
+//                         console.log("a'lo");
+                        
+//                         break;
+
+//     default:
+//         console.log("xato");
+        
+//         break;
+// }
+
+
+
+                                    // 3-masala
+
+// let a=+prompt("oyning tartib raqamini kiritind")
+// switch (a) {
+//     case 12:
+//         case 1:
+//             case 2:
+//         console.log("Qish");
+//         break;
+//         case 3:
+//             case 4:
+//                 case 5:
+//             console.log("Baxor");
+//             break;
+//         case 6:
+//         case 7:
+//             case 8:
+//         console.log("Yoz");
+//         break;
+//         case 9:
+//             case 10:
+//                 case 11:
+//             console.log("Kuz");
+//             break;
+    
+    
+//     default:
+//         console.log("Bunday oy yo'q");
+        
+//         break;
+// }
+
+                            // 4-masala
+
+// let a=+prompt("oy tartib raqamini kiriting")
+// switch (a) {
+//     case 1:
+//         case 3:
+//             case 5:
+//                 case 7:
+//                     case 8:
+//                         case 10:
+//                             case 12:
+//      alert("31 kundan iborat")
+//         break;
+//         case 4:
+//             case 6:
+//                 case 9:
+//                     case 11:
+//     alert("30 kundan iborat")
+//     case 2:
+//         alert("kabisa yilida 29 kun qolgan yillarda 28 kun")
+
+
+//     default:
+//         alert("kechirasiz bunday qiymat aniqlanmadi")
+//         break;
+// }
+
+                            // 5-masala
+
+// let A=+prompt("birinchi sonni kiriting")
+// let B=+prompt("ikkinchi sonni kiriting")
+// let x=+prompt("amal bajarish uchun 1 dan 4 gacha raqamlardan birini kiriting")
+// switch (x) {
+//     case 1:
+//         alert(A+B)
+//         break;
+
+//     case 2:
+//         alert(A-B)
+//         break;
+
+//     case 3:
+
+//         alert(A/B)
+//         break;
+
+//      case 4:
+//          alert(A*B)
+        
+//         break;
+
+//     default:
+//         alert("iltimos amal bajarish oynasiga faqat 1 dan 4 gacha raqam kiriting")
+//         break;
+// }
+
+                            // 6-masala
+
+// let a=+prompt("qiymatni kiriting")
+// let b=+prompt("ditsimetr uchum 1. kilometr uchun 2. metr uchun 3. millimetr uchun 4. santimetr uchun 5 ni kiriting")
+// switch (b) {
+//     case 1:
+//         alert(a/10 +" metr")
+//         break;
+//     case 2:
+//          alert(a*1000 +" metr")
+//         break;
+//      case 3:
+//         alert(a +" metr")
+//         break;
+//     case 4:
+//         alert(a/1000 +" metr")
+//         break;
+//     case 5:
+//         alert(a/100 +" metr")
+//         break;       
+
+//     default:
+    //    alert("kechirasiz bunday qiymat aniqlanmadi")
+//         break;
+// }
+
+                        // 7-masala
+
+// let a=+prompt("qiymatni kiriting")
+// let b=+prompt("kilogram uchum 1. milligram uchun 2. gram uchun 3. tonna uchun 4. sentner uchun 5 ni kiriting")
+// switch (b) {
+//     case 1:
+//         alert(a +" kilogram")
+//         break;
+//     case 2:
+//             alert(a/1000000 +" kilogram")
+//         break;
+//         case 3:
+//         alert(a/1000 +" kilogram")
+//         break;
+//     case 4:
+//         alert(a*1000 +" kilogram")
+//         break;
+//     case 5:
+//         alert(a*100 +" kilogram")
+//         break;       
+
+//     default:
+//         alert("kechirasiz bunday qiymat aniqlanmadi")
+//         break;
+// }
+
+                            // 8-masala
+
+// let D=+prompt("kunni kiriting")
+// let M=+prompt("Oyni kiriting")
+// switch (M) {
+//     case 1:
+//         case 3:
+//             case 5:
+//                 case 7:
+//                     case 8:
+//                         case 10:
+//                          case 12:
+//     if (D>0 && D<=31) {
+//         alert(D+"," +M)
+//     }else {
+//         alert("bu oyda bunday kun yo'q")
+//     }
+//         break;
+//         case 4:
+//             case 6:
+//                 case 9:
+//                     case 11:
+//         if (D>0 && D<=30) {
+//             alert(D+"," +M)
+//         }else {
+//             alert("bu oyda bunday kun yo'q")
+//         }   
+//     case 2:
+//         if (D>0 && D<=28) {
+//             alert(D+"," +M)
+//         }else {
+//             alert("bu oyda bunday kun yo'q")
+//         }
+
+//     default:
+//         alert("kechirasiz bunday kun aniqlanmadi")
+//         break;
+// }
+
+                        // 9-masala
+
+// let D=+prompt("kunni kiriting")
+// let M=+prompt("Oyni kiriting")
+// switch (M) {
+//     case 1:
+//         case 3:
+//             case 5:
+//                 case 7:
+//                     case 8:
+//                         case 10:
+//     if (D>0 && D<31) {
+//         alert((D+1)+"." +M)
+//     }else if(D==31){
+//         alert((D=1)+"." +(M+1))
+//     } else {
+//         alert("bu oyda bunday kun yo'q")
+//     }
+//         break;
+//         case 4:
+//             case 6:
+//                 case 9:
+//                     case 11:
+//                         if (D>0 && D<30) {
+//                             alert((D+1)+"." +M)
+//                         }else if(D==30){
+//                             alert((D=1)+"." +(M+1))
+//         }else {
+//             alert("bu oyda bunday kun yo'q")
+//         }   
+//         break;
+//     case 2:
+//         if (D>0 && D<28) {
+//             alert((D+1)+"." +M)
+//         }else if(D==28) {
+//             alert((D=1)+"." +(M+1))
+//         }else {
+//             alert("bu oyda bunday kun yo'q")
+//         }
+//         break;
+//         case 12:
+//             if (D>0 && D<31) {
+//                 alert((D+1)+"." +M)
+//             }else if(D==31){
+//                 alert((D=1)+"." +(M=1))
+//             } else {
+//                 alert("bu oyda bunday kun yo'q")
+//             }
+//                 break;
+//     default:
+//         alert("kechirasiz bunday kun aniqlanmadi")
+//         break;
+// }
+
+                            // 10-masala
+
+// let Y=prompt("Robot yo'nalishi. s-shimol, j-janub, q-sharq, g-g'arb")
+// let K=+prompt("Komanda")
+// if (Y=="s") {
+//     switch (K) {
+//         case 0:
+//             alert("Shimol ")
+//             break;
+//             case 1:
+//             alert("Shimoldan G'arbga")
+//             break;
+//             case 2:
+//             alert("Shimoldan Sharqqa")
+//             break;
+//         default:
+//             alert("Komandaga faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//             break;}
+//     }else if (Y=="j") {
+//         switch (K) {
+//             case 0:
+//                 alert("Janub ")
+//                 break;
+//                 case 1:
+//                 alert("Janubdan sharqqa")
+//                 break;
+//                 case 2:
+//                 alert("Janubdan g'arbga")
+//                 break;
+//             default:
+//                 alert("Komandaga faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                 break;}
+//         }else if (Y=="q") {
+//             switch (K) {
+//                 case 0:
+//                     alert("sharqqa ")
+//                     break;
+//                     case 1:
+//                     alert("sharqdan shimolga")
+//                     break;
+//                     case 2:
+//                     alert("Sharqdan janubga")
+//                     break;
+//                 default:
+//                     alert("Komandaga faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                     break;}
+//             }else if (Y=="g") {
+//                 switch (K) {
+//                     case 0:
+//                         alert("G'arbga ")
+//                         break;
+//                         case 1:
+//                         alert(" G'arbdan janubga")
+//                         break;
+//                         case 2:
+//                         alert("G'arbdan Shimolga")
+//                         break;
+//                     default:
+//                         alert("Komandaga faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                         break;}
+//                 }else {
+//                     alert("iltimos yo'nalishni to'g'ri kiriting")
+//                 }
+
+                    // 11-masala   
+
+// let C=prompt("Robot yo'nalishi. s-shimol, j-janub, q-sharq, g-g'arb")
+// let K1=+prompt("Komanda birinchi")
+// let K2=+prompt("Komanda ikkinchi")
+// if (C=="s") {
+//     switch (K1) {
+//         case 0:
+//             switch (K2) {
+//                 case 0:
+//                     alert("janubdan g'arbga ")
+//                     break;
+//                 case 1:
+//                     alert("janubdan sharqqa ") 
+//                     break;
+//                 case 2:
+//                     alert("shimol")
+//                     break;
+            
+//                 default:
+//                     alert("iltimos komanda ikkinchini  to'g'ri bering")
+//                     break;
+//             }
+//             break;
+//             case 1:
+//                 switch (K2) {
+//                     case 0:
+//                         alert("shimol ")
+//                         break;
+//                     case 1:
+//                         alert("janub ") 
+//                         break;
+//                     case 2:
+//                         alert("sharqqa")
+//                         break;
+                
+//                     default:
+//                         alert("iltimos komanda ikkinchini to'g'ri bering")
+//                         break;
+//                 }
+//             break;
+//             case 2:
+//                 switch (K2) {
+//                     case 0:
+//                         alert("g'arb ")
+//                         break;
+//                     case 1:
+//                         alert("sharq") 
+//                         break;
+//                     case 2:
+//                         alert("shimol")
+//                         break;
+                
+//                     default:
+//                         alert("iltimos komanda ikkinchini to'g'ri bering")
+//                         break;
+//                 }
+//             break;
+//         default:
+//             alert("Komanda birinchida faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//             break;}
+
+            
+//     }else if (C=="j") {
+//         switch (K1) {
+//             case 0:
+//                 switch (K2) {
+//                     case 0:
+//                         alert("shimol ")
+//                         break;
+//                     case 1:
+//                         alert("janub") 
+//                         break;
+//                     case 2:
+//                         alert("sharq")
+//                         break;
+                
+//                     default:
+//                         alert("iltimos komanda ikkinchini  to'g'ri bering")
+//                         break;
+//                 }
+//                 break;
+//                 case 1:
+//                     switch (K2) {
+//                         case 0:
+//                             alert("janub ")
+//                             break;
+//                         case 1:
+//                             alert("shimol ")
+//                             break; 
+//                         case 2:
+//                             alert("g'arb")
+//                             break;
+                    
+//                         default:
+//                             alert("iltimos komanda ikkinchini to'g'ri bering")
+//                             break;
+//                     }
+//                 break;
+//                 case 2:
+//                     switch (K2) {
+//                         case 0:
+//                             alert("g'arb ")
+//                             break;
+//                         case 1:
+//                             alert("sharq") 
+//                             break;
+//                         case 2:
+//                             alert("shimol")
+//                             break;
+                    
+//                         default:
+//                             alert("iltimos komanda ikkinchini to'g'ri bering")
+//                             break;
+//                     }
+//                 break;
+//             default:
+//                 alert("Komanda birinchida faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                 break;}
+//         }else if (C=="q") {
+//             switch (K1) {
+//                 case 0:
+//                     switch (K2) {
+//                         case 0:
+//                             alert("g'arb ")
+//                             break;
+//                         case 1:
+//                             alert("sharq") 
+//                             break;
+//                         case 2:
+//                             alert("shimol")
+//                             break;
+                    
+//                         default:
+//                             alert("iltimos komanda ikkinchini  to'g'ri bering")
+//                             break;
+//                     }
+//                     break;
+//                     case 1:
+//                         switch (K2) {
+//                             case 0:
+//                                 alert("sharq ")
+//                                 break;
+//                             case 1:
+//                                 alert("g'arb ") 
+//                                 break;
+//                             case 2:
+//                                 alert("janub")
+//                                 break;
+                        
+//                             default:
+//                                 alert("iltimos komanda ikkinchini to'g'ri bering")
+//                                 break;
+//                         }
+//                     break;
+//                     case 2:
+//                         switch (K2) {
+//                             case 0:
+//                                 alert("shimol ")
+//                                 break;
+//                             case 1:
+//                                 alert("janub")
+//                                 break; 
+//                             case 2:
+//                                 alert("sharq")
+//                                 break;
+                        
+//                             default:
+//                                 alert("iltimos komanda ikkinchini to'g'ri bering")
+//                                 break;
+//                         }
+//                     break;
+//                 default:
+//                     alert("Komanda birinchida faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                     break;}
+//             }else if (C=="g") {
+//                 switch (K1) {
+//                     case 0:
+//                         switch (K2) {
+//                             case 0:
+//                                 alert("sharq ")
+//                                 break;
+//                             case 1:
+//                                 alert("g'arb") 
+//                                 break;
+//                             case 2:
+//                                 alert("janub")
+//                                 break;
+                        
+//                             default:
+//                                 alert("iltimos komanda ikkinchini  to'g'ri bering")
+//                                 break;
+//                         }
+//                         break;
+//                         case 1:
+//                             switch (K2) {
+//                                 case 0:
+//                                     alert("g'arb ")
+//                                     break;
+//                                 case 1:
+//                                     alert("sharq") 
+//                                     break;
+//                                 case 2:
+//                                     alert("shimol")
+//                                     break;
+                            
+//                                 default:
+//                                     alert("iltimos komanda ikkinchini to'g'ri bering")
+//                                     break;
+//                             }
+//                         break;
+//                         case 2:
+//                             switch (K2) {
+//                                 case 0:
+//                                     alert("janub")
+//                                     break;
+//                                 case 1:
+//                                     alert("shimol") 
+//                                     break;
+//                                 case 2:
+//                                     alert("g'arb")
+//                                     break;
+//                                 default:
+//                                     alert("iltimos komanda ikkinchini to'g'ri bering")
+//                                     break;
+//                             }
+//                         break;
+//                     default:
+//                         alert("Komanda birinchida faqat 0, 1, 2 qiymatlarini kiritish mumkin")
+//                         break;}
+//             }else {
+//                 alert("yo'nalishni xato kiritdingiz")
+//             }
